84beb91213483de56928a8536f8a37c68223c0fd
Vinícius Vasques <vinivasq@hotmail.com>
2023-04-09 15:31:17 -0300

    informações do perfil

---
+++ src/components/ProfileBio.js
@@ -0,0 +1,43 @@
+import { Box, Image } from "@chakra-ui/react";
+import ProfilePic from "../assets/images/doggos/userpic.png";
+import React from "react";
+import Text from "./Text";
+
+const ProfileBio = () => {
+  return (
+    <Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+        gap="1rem"
+        padding="1rem 0"
+      >
+        <Image
+          src={ProfilePic}
+          alt="foto de perfil"
+          boxSize={73}
+          boxShadow="0px 1px 3px #0000006b"
+          borderRadius="full"
+        />
+        <Box width="">
+          <Text as="h1" fontSize="1.375rem" fontWeight="700">
+            Bill Bulldog
+          </Text>
+          <Text letterSpacing="-0.3px" color="#687684">
+            @billthebulldog2022
+          </Text>
+        </Box>
+      </Box>
+      <Text
+        width="min"
+        padding="0.175rem 0.5rem"
+        borderBottom="3px solid #00ACC1"
+      >
+        Petposts
+      </Text>
+    </Box>
+  );
+};
+
+export default ProfileBio;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,21 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Text from "../components/Text";
-import ProfilePic from "../assets/images/doggos/userpic.png";
-import { Box, Image } from "@chakra-ui/react";
+import ProfileBio from "../components/ProfileBio";
 
 const Profile = () => {
   return (
     <>
-      <Navbar />
       <header>
-        <Box>
-          <Image src={ProfilePic} alt="foto de perfil" boxSize={73} />
-          <Box>
-            <Text as="h1">Bill Bulldog</Text>
-            <Text>@billthebulldog2022</Text>
-          </Box>
-        </Box>
+        <Navbar />
+        <ProfileBio />
       </header>
     </>
   );
