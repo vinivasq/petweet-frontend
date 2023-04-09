cbe744f2d91888fc7d61ba61253ba942e45615de
Vinícius Vasques <vinivasq@hotmail.com>
2023-04-09 17:33:42 -0300

    cabeçalho de criação de post

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,7 +12,11 @@ const Button = (props) => {
       fontWeight="600"
       color={variant === "outline" ? "#00acc1" : "white"}
       backgroundColor={variant === "outline" ? "transparent" : "#00acc1"}
+      boxShadow="2px 4px 4px rgba(0, 0, 0, 0.09)"
       border={variant === "outline" ? "1px solid #00acc1" : "none"}
+      _hover={{
+        backgroundColor: variant === "outline" ? "#00acc110" : "#00acc1ba",
+      }}
       {...rest}
     />
   );
--- src/routes/CreatePost.js
+++ src/routes/CreatePost.js
@@ -1,7 +1,33 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import Text from "../components/Text";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <>
+      <Box
+        boxShadow="0px 1px 3px #00000030"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        padding="1rem"
+      >
+        <Link to="/home">
+          <Text fontWeight="300" fontSize="0.75rem" color="#424242">
+            Cancelar
+          </Text>
+        </Link>
+        <Box display="flex" alignItems="center" gap="0.875rem">
+          <Text color="#828282" fontSize="0.875rem">
+            0/140
+          </Text>
+          <Button borderRadius="0.625rem">Petwittar</Button>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default CreatePost;
