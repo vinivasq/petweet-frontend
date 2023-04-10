c9eab643d5d165c77dc78a5e78edc2dd848bedc1
Vinícius Vasques <vinivasq@hotmail.com>
2023-04-10 17:18:50 -0300

    contador de caracteres dinamico

--- src/routes/CreatePost.js
+++ src/routes/CreatePost.js
@@ -1,11 +1,15 @@
 import { Box, Image, Textarea } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import Text from "../components/Text";
 import ProfilePic from "../assets/images/doggos/userpic.png";
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
+import { useAuth } from "../context/auth-context";
 
 const CreatePost = () => {
+  const { user } = useAuth();
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <Box
@@ -22,9 +26,11 @@ const CreatePost = () => {
         </Link>
         <Box display="flex" alignItems="center" gap="0.875rem">
           <Text color="#828282" fontSize="0.875rem">
-            0/140
+            {count}/140
           </Text>
-          <Button borderRadius="0.625rem">Petwittar</Button>
+          <Button borderRadius="0.625rem" onClick={() => console.log(user.id)}>
+            Petwittar
+          </Button>
         </Box>
       </Box>
       <Box padding="0.375rem 1rem" display="flex">
@@ -40,8 +46,13 @@ const CreatePost = () => {
           resize="none"
           focusBorderColor="transparent"
           placeholder="O que está acontecendo?"
+          maxLength="140"
+          height="8rem"
           _placeholder={{ color: "#687684" }}
           fontFamily="Open Sans"
+          onChange={(e) => {
+            setCount(e.target.value.length);
+          }}
         />
       </Box>
     </>
